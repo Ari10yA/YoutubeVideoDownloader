c063f2250ace3032e9bf95d6245baf0ffae02331
Ari10yA <aritya_201900397@smit.smu.edu.in>
2023-09-18 18:42:04 +0530

    working_copy2

--- main.js
+++ main.js
@@ -1,35 +1,60 @@
-
+const express = require('express');
+const ytdl = require('ytdl-core');
 const ffmpegPath = require('ffmpeg-static');
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
 
-const express = require('express');
-const ytdl = require('ytdl-core');
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Map quality options to corresponding itag values
-const qualityOptions = {
-    highest: 'highest',
-    lowest: 'lowest',
-    '720p': '22', // Video - 720p
-    '360p': '18', // Video - 360p
-    audio: 'audioonly', // Audio (MP3)
+
+app.get('/getQualities', async (req, res) => {
+    const videoUrl = req.query.url;
+
+    try {
+        if (!ytdl.validateURL(videoUrl)) {
+            return res.status(400).json({ error: 'Invalid YouTube URL' });
+        }
+
+        const info = await ytdl.getInfo(videoUrl);
+        const formats = info.formats.filter((format) => format.container === 'mp4');
+        const qualityLabels = formats.map((format) => format.qualityLabel);
+        res.json(qualityLabels);
+    } catch (error) {
+        console.error('Error fetching quality options:', error);
+        res.status(500).json({ error: 'Error fetching quality options' });
+    }
+});
+
+const qualityLabelsToItags = {
+    'highest': 'highest',
+    'lowest': 'lowest',
+    'audio': 'audioonly', 
+    '2160p (4k)': '313', 
+    '1440p (2k)': '271', 
+    '1080p (Full HD)': '137', 
+    '720p (HD)': '22', 
+    '480p': '135', 
+    '360p': '18', 
+    '240p': '133', 
+    '144p': '160', 
 };
+
 
 app.get('/download', async (req, res) => {
     const videoUrl = req.query.url;
-    const selectedQuality = req.query.quality;
+    const selectedType = req.query.type;
+    const selectedQualityLabel = req.query.quality;
 
     try {
         const info = await ytdl.getInfo(videoUrl);
+        const videoTitle = info.videoDetails.title;
 
-        if (selectedQuality === 'audio') {
-            const videoTitle = info.videoDetails.title;
-
+        if (selectedType === 'audio') {
             const audioFormat = ytdl.chooseFormat(info.formats, { filter: 'audioonly' });
 
             if (audioFormat) {
@@ -43,20 +68,19 @@ app.get('/download', async (req, res) => {
                 res.status(400).send('No matching audio format found.');
             }
         } else {
-            const itag = qualityOptions[selectedQuality];
-            const videoTitle = info.videoDetails.title;
+           
+            const videoFormats = info.formats.filter((format) => format.hasVideo && format.hasAudio);
+            const selectedFormat = videoFormats.find((format) => format.qualityLabel === selectedQualityLabel);
 
-            const videoFormat = ytdl.chooseFormat(info.formats, { quality: itag });
-
-            if (videoFormat) {
+            if (selectedFormat) {
                 res.header('Content-Disposition', `attachment; filename="${videoTitle}.mp4"`);
-                ytdl(videoUrl, { format: videoFormat })
+                ytdl(videoUrl, { format: selectedFormat })
                     .pipe(res)
                     .on('finish', () => {
                         console.log('Video download finished');
                     });
             } else {
-                res.status(400).send('No matching video format found.');
+                res.status(400).send('No matching video format found with both video and audio.');
             }
         }
     } catch (error) {
@@ -68,6 +92,3 @@ app.get('/download', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-
-
--- public/script.js
+++ public/script.js
@@ -1,24 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     const downloadBtn = document.getElementById('download-btn');
     const urlInput = document.getElementById('url');
+    const typeSelect = document.getElementById('type');
     const qualitySelect = document.getElementById('quality');
     const downloadLink = document.getElementById('download-link');
     const downloadLinkA = document.getElementById('download-link-a');
     const errorMessage = document.getElementById('error-message');
 
+    async function updateQualityOptions() {
+        const selectedType = typeSelect.value;
+        const videoUrl = urlInput.value;
+    
+        if (videoUrl.trim() === '') {
+            return;
+        }
+    
+        try {
+            const response = await fetch(`/getQualities?url=${videoUrl}`);
+            if (response.ok) {
+                const qualityOptions = await response.json();
+                qualitySelect.innerHTML = '';
+    
+                qualityOptions.forEach((quality) => {
+                    const option = document.createElement('option');
+                    option.value = quality;
+                    option.textContent = quality;
+                    qualitySelect.appendChild(option);
+                });
+            } else {
+                errorMessage.textContent = 'Error fetching quality options.';
+            }
+        } catch (error) {
+            console.error('Error fetching quality options:', error);
+            qualitySelect.innerHTML = '';
+        }
+    }
+
+    typeSelect.addEventListener('change', updateQualityOptions);
+    urlInput.addEventListener('input', updateQualityOptions);
+
     downloadBtn.addEventListener('click', async () => {
         const videoUrl = urlInput.value;
+        const selectedType = typeSelect.value;
         const selectedQuality = qualitySelect.value;
 
         try {
-            const response = await fetch(`/download?url=${videoUrl}&quality=${selectedQuality}`);
+            const response = await fetch(`/download?url=${videoUrl}&type=${selectedType}&quality=${selectedQuality}`);
             if (response.ok) {
                 const blob = await response.blob();
                 const objectURL = URL.createObjectURL(blob);
 
                 downloadLinkA.href = objectURL;
                 downloadLinkA.style.display = 'block';
-                downloadLinkA.download = `${videoUrl}.${selectedQuality === 'audio' ? 'mp3' : 'mp4'}`;
+                downloadLinkA.download = `${videoUrl}.${selectedType === 'video' ? 'mp4' : 'mp3'}`;
 
                 errorMessage.textContent = '';
             } else {
@@ -28,4 +62,6 @@ document.addEventListener('DOMContentLoaded', () => {
             errorMessage.textContent = 'Error processing the request.';
         }
     });
+
+    updateQualityOptions();
 });
